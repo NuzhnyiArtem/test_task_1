f543db79e7ff746ec408266771ea6c131a0952d4
NuzhnyiArtem <tom-corey@yandex.ru>
2023-03-21 22:16:45 +0300

    test_task_1

--- find_matches.py
+++ find_matches.py
@@ -24,31 +24,40 @@ def method_decorator(func: Callable):
 
 
 class Rules:
-
+    """Принимает на вход путь к модели, имеет изменяемые параметры:
+    model_type: str - Тип модели. По умолчанию установлен 'yolov5'.
+    confidence_threshold: float - Пороговое значение. По умолчанию установлен 0,85.
+    device: str - Инференс модели будет работать на CPU или GPU. По умолчанию установлен 'cpu'.
+    slice_height: int - Высота среза изображения. По умолчанию установлен 640.
+    slice_width: int - Ширина среза изображения. По умолчанию установлен 640.
+    overlap_height_ratio: float - Перекрытие среза по высоте. По умолчанию установлен 0.2
+    overlap_width_ratio: float - Перекрытие среза по ширине. По умолчанию установлен 0.2
+    """
     def __init__(self, path: str) -> None:
         self.path = path
 
     @method_decorator
     def find_matches(self, image: str) -> Dict[str, List[List[int]]]:
+        
         try:
             detection_model = AutoDetectionModel.from_pretrained(
                 model_type='yolov5',
                 model_path=self.path,
-                confidence_threshold=0.7,
+                confidence_threshold=0.85,
                 device="cpu",
             )
 
             result = get_sliced_prediction(
                 image,
                 detection_model,
-                slice_height=1280,
-                slice_width=1280,
+                slice_height=640,
+                slice_width=640,
                 overlap_height_ratio=0.2,
                 overlap_width_ratio=0.2
             )
             res = defaultdict(list)
             result.export_visuals(export_dir="demo_data/", text_size=0.5, rect_th=2)
-            q = result.to_coco_predictions(image_id=1)
+            q = result.to_coco_predictions()
 
             for rs in q:
                 res[rs['category_name']].append(rs['bbox'])
@@ -58,7 +67,7 @@ class Rules:
             logger.exception(e)
 
 
-yolov5_model_path = r'C:\Users\mlwk\PycharmProjects\marking\testsplit\result3\weights\best.pt'
+yolov5_model_path = r'C:\Users\mlwk\PycharmProjects\marking\testsplit\result5\weights\best.pt'
 image = askopenfilename()
 r = Rules(yolov5_model_path)
 r.find_matches(image)
